9741ad764b21aac5ef8b9d4c0557199132dcaad2
moringa <charonkobarix@yahoo.com>
2019-08-13 08:13:41 +0300

    imported manager, shell, server to render it on the manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app, db
-from flask_script import Manager, Shell, Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server, Shell
 
 # Creating app instance
 
